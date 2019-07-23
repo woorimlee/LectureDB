5d7a0ae4da318508cb1342bea9738a0a0f60e921
woorimlee <and_lamyland@naver.com>
2019-07-23 17:59:14 +0900

    잠시 다시 공부 해봤어

---
+++ 나도 공부/study/study/포인터스왑.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void SwapIntPtr(int** p1, int** p2)
+{
+	int* temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+void SwapIntPtr2(int** p1, int** p2);
+
+int main(void) {
+
+	int num1 = 10, num2 = 20;
+	int* ptr1, * ptr2;
+	ptr1 = &num1, ptr2 = &num2;
+	cout << "ptr1 : " << &num1 << " ptr2 : " << &num2 << endl;
+	cout << "ptr1 : " << ptr1 << " ptr2 : " << ptr2 << endl;
+	SwapIntPtr(&ptr1, &ptr2);
+	cout << "ptr1 : " << ptr1 << " ptr2 : " << ptr2 << endl;
+	cout << "ptr1 : " << *ptr1 << " ptr2 : " << *ptr2 << endl;
+	return 0;
+}
+
+void SwapIntPtr2(int** p1, int** p2)
+{
+	int temp = **p1;
+	**p1 = **p2;
+	**p2 = temp;
+}
